8f4aa33c8ed42a9cc99f85b027e790c80a401683
Iamtodor <todor.ilya@gmail.com>
2015-12-07 11:59:08 +0200

    7december.Fixes.1_1

--- app/src/main/java/com/todor/diabetes/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.todor.diabetes.utils;
-
-public class Constants {
-
-    public static final String MY_PREFERENCES = "com.todor.diabetes";
-    public static final String IS_FIRST_LAUNCH_KEY = "isFirstLaunch";
-
-}
