2b375ddc35b2a8517c4124fec52ee1dd831f16e1
Iamtodor <todor.ilya@gmail.com>
2016-02-07 15:15:02 +0200

    Added card view in XML with no functionality

--- app/src/main/java/com/todor/diabetes/ui/MainActivity.java
+++ app/src/main/java/com/todor/diabetes/ui/MainActivity.java
@@ -2,23 +2,17 @@ package com.todor.diabetes.ui;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.app.SearchManager;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 
 import com.todor.diabetes.R;
-import com.todor.diabetes.db.DbHelperSingleton;
 import com.todor.diabetes.ui.product_list.ProductListFragment;
 import com.todor.diabetes.ui.product_table.ProductTableFragment;
 import com.todor.diabetes.ui.profile.ProfileFragment;
